64cc56c2e4fd916cfed8f67c3dd0f6e35ad0c361
Flawden <topchiidv1998@gmail.com>
2024-06-20 20:54:12 +0300

    был составлен файл с SQL запросами в соответствии с заданием

---
+++ scripts.sql
@@ -0,0 +1,6 @@
+SELECT * FROM student;
+SELECT * FROM student WHERE age BETWEEN 10 AND 20;
+SELECT name FROM student;
+SELECT * FROM student WHERE lower(name) LIKE '%o%';
+SELECT * FROM student WHERE age < id;
+SELECT * FROM student ORDER BY age;
